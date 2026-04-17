fd9b1d2a80a4667cd2e83dd9bcc4460c8aa9ffea
Vraj04 <vrajp800@gmail.com>
2026-04-16 21:32:25 -0400

    Fix the /api error

--- v-fit/backend/src/main/java/com/vfit/shared/config/SecurityConfig.java
+++ v-fit/backend/src/main/java/com/vfit/shared/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://18.218.225.149"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
